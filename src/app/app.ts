3f19ff8c9e3452ee03262c79677ad64da27f32df
Alejandro Daniel López Orona <alejandro_3041220281@utd.edu.mx>
2025-11-13 00:33:58 -0600

    scroll y desactivar animacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, AfterViewInit, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,30 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('cv');
+  
+  // Opción para activar/desactivar animaciones (cambiar a false para desarrollo)
+  protected readonly enableAnimations = false;
+  
+  @HostBinding('class.animations-disabled') get animationsDisabled() {
+    return !this.enableAnimations;
+  }
+  
+  protected scrollBlocked = signal(true);
+
+  ngAfterViewInit() {
+    if (this.enableAnimations) {
+      // La animación más larga termina a los 2.6s + 0.6s de duración = 3.2s
+      // Agregamos un pequeño margen de seguridad
+      setTimeout(() => {
+        this.scrollBlocked.set(false);
+      }, 3300);
+    } else {
+      // Si las animaciones están desactivadas, desbloquear inmediatamente
+      setTimeout(() => {
+        this.scrollBlocked.set(false);
+      }, 0);
+    }
+  }
 }
